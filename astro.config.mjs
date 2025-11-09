b93d3a44a6517de7a33e3d3bf316526f3975685b
AlexanderPM11 <84640834+AlexanderPM11@users.noreply.github.com>
2025-11-09 17:21:47 -0400

    sitemap conifg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://apolanco.com",
   integrations: [
+    sitemap(),
     icon({
       // Especificar los icon sets que quieres usar
       iconDir: "src/icons",
